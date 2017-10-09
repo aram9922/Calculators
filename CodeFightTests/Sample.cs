3eb474a5bbb02829dbf9f99484b61d6ce4e8f3bf
Nshan Barsegyan <nshanb@outlook.com>
2017-10-10 00:41:28 +0400

    almostIncreasingSequence

--- CodeFightTests/Sample.cs
+++ CodeFightTests/Sample.cs
@@ -22,24 +22,27 @@ namespace CodeFightTests
         [TestMethod]
         public void TestMethod1()
         {
-            almostIncreasingSequence(new int[] { 1, 3, 2 });
+            bool b = almostIncreasingSequence(new int[] { 1, 3, 2 });
+            Assert.IsTrue(b);
+            b = almostIncreasingSequence(new int[] { 1, 1 });
+            Assert.IsTrue(b);
         }
         bool almostIncreasingSequence(int[] sequence)
         {
             int error = 0;
             bool error1 = false;
             bool error2 = false;
-            for (int i = 0; i <= sequence.Length - 1; i++)
+            for (int i = 0; i < sequence.Length - 1; i++)
             {
-                if (sequence[i] > sequence[i + 1] && error != 1)
+                if (sequence[i] >= sequence[i + 1] && error != 1)
                 {
                     error = error + 1;
-                    if (error1 = sequence[i - 1] < sequence[i + 1])
+                    if (error1 = i == 0 || sequence[i - 1] < sequence[i + 1])
                     {
                         ;
                         continue;
                     }
-                    if (error2 = sequence[i] < sequence[i + 2])
+                    if ( error2 = i >= sequence.Length - 2 || sequence[i] < sequence[i + 2])
                     {
                         ;
                         continue;
@@ -55,6 +58,7 @@ namespace CodeFightTests
                 }
                 else { return false; }
             }
+            return error<=1;
         }
     }
 }
